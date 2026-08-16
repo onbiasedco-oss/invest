14bac95ae3f1cbc56984511667b6d44d72b0376b
onbiasedco-oss <onbiasedco@gmail.com>
2026-08-16 18:25:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/invest/', // Replace with your exact GitHub repository name
+})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
