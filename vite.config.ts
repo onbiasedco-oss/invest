b7506ec69a7510e01f96a35e441323038bd06693
onbiasedco-oss <onbiasedco@gmail.com>
2026-08-16 18:31:50 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/invest/', // Replace with your exact GitHub repository name
+  base: 'https://github.com/onbiasedco-oss/invest.git', // Replace with your exact GitHub repository name
 })
 
 // https://vitejs.dev/config/
